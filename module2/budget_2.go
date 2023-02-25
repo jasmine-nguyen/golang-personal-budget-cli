5f8b25fdec8892ab535876172434182dc52e786f
Jasmine Nguyen <jasminel.ngn@gmail.com>
2023-02-25 20:47:50 +1100

    Reorder code

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -73,12 +73,15 @@ func (b *Budget) RemoveItem(description string) {
 
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
+	var newBudget *Budget
 	if len(report) >= 12 {
 		return nil, errReportIsFull
 	}
-	var newBudget *Budget
-	budget := &Budget{Max: max}
-	newBudget = budget
+	if _, hasEntry := report[month]; hasEntry {
+		return nil, errDuplicateEntry
+	}
+	newBudget = &Budget{Max: max}
+
 	report[month] = newBudget
 	return newBudget, nil
 }
